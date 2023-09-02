8cb7408fffe761ac3c4fbbc501524aacf19d7e5f
ShivamKsolves <113895013+ShivamKsolves@users.noreply.github.com>
2023-09-02 06:54:16 +0000

    datatypes and varaible

---
+++ .devcontainer/o1basics/01variable.js
@@ -0,0 +1,23 @@
+const accountId = 14445
+let accountemail="shivam@gmail.com"
+var password = "1234"
+accountCity ="Indore"
+let accountSate;
+// accountSate will be undefind 
+
+// accountId =2; Not allowed
+accountemail ="sh@gmail.com"
+password ="4442"
+accountCity="Bangaloure"
+
+
+/*
+prefer not use var 
+because of issue in  block scope and funcational scope
+*/
+
+
+// console.log(accountId);
+
+// Eak time pe sare variable ko use kar sakte hai 
+console.table([accountId, accountemail, password, accountCity, accountSate]);
---
+++ .devcontainer/o1basics/02datatypes.js
@@ -0,0 +1,33 @@
+"use strict" //Terat all js code with newer version
+
+// alert(3+3 ) we are using node.js not browser
+
+// console.log(3+3);console.log("shivam");
+
+let name="shivam"
+let age=22;
+let isLoggedIn = false
+
+
+// console.table([name, age,isLoggedIn]);
+
+console.log(typeof "shivam");
+
+console.log(typeof null); // object
+
+console.log(typeof undefined);
+
+// Premitive datatypes
+// Number
+// BigInt
+// String
+// Boolean
+// null - standalone value
+// undefined - Value not defind
+// sumbol - unique 
+
+
+// Nonpremitives
+
+// object
+
