f083bbbdd0d98f153763cbe70a1158487d3349d2
ShivamKsolves <113895013+ShivamKsolves@users.noreply.github.com>
2023-09-02 07:16:06 +0000

    operations

--- .devcontainer/o1basics/03conversion.js
+++ .devcontainer/o1basics/03conversion.js
@@ -18,5 +18,36 @@ let booleabLoggedIn = Boolean(loggedIn)
 let  number=33
 
 let stringNumber = String(number)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+
+//  OPerations
+
+let value = 3;
+let negvalue = -value
+
+
+// console.log(negvalue);
+// console.log(5/3);
+// console.log(5%3);
+// console.log(2**3);
+
+let str1="shivam"
+let str2 ="kumar"
+
+// console.log(str1+str2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+// console.log(true); // true
+// console.log(+true); //1
+// console.log(true+); // error
+
+
+let counter = 12;
+// ++counter
+counter++
+console.log(counter);
