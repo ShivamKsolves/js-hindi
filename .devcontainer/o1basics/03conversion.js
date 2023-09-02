f96bb036f9151da887f370e4300c974cdb468622
ShivamKsolves <113895013+ShivamKsolves@users.noreply.github.com>
2023-09-02 07:05:09 +0000

    conversion

---
+++ .devcontainer/o1basics/03conversion.js
@@ -0,0 +1,16 @@
+// let score ="33abc"
+let score = null
+// let score = undefind
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInt = Number(score)  // change string to number
+// console.log(valueInt);
+// console.log(typeof(valueInt));
+
+let loggedIn = 1;
+
+let booleabLoggedIn = Boolean(loggedIn)
+
+console.log(booleabLoggedIn);
