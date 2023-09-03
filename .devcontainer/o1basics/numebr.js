129665758086c09bf98c1e888332d1510ad0558f
ShivamKsolves <113895013+ShivamKsolves@users.noreply.github.com>
2023-09-03 10:15:34 +0000

    Number and Maths

---
+++ .devcontainer/o1basics/numebr.js
@@ -0,0 +1,43 @@
+const score =400
+// console.log(score);
+
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString.length);
+// console.log(balance.toFixed(2));
+
+// console.log(`Balance is ${balance} ant type is ${typeof(balance)}`);
+// console.log('shivam');
+
+// const otherNumber = 23.865
+
+// console.log(otherNumber.toPrecision(2));
+
+
+const  hunderdes = 1000000
+
+// console.log(hunderdes.toLocaleString());
+// console.log(hunderdes.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.7));
+// console.log(Math.sqrt(5));
+// console.log(Math.min(10,23,34));
+// console.log(Math.random());
+// console.log(Math.random()*10 +1);
+// console.log((Math.random()*10)+1);
+
+const min= 10
+const max =20
+
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
