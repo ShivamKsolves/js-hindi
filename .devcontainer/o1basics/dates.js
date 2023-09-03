31310c946da4f4552419c1c049cceacacca71fa3
ShivamKsolves <113895013+ShivamKsolves@users.noreply.github.com>
2023-09-03 10:35:08 +0000

    Date and Time

---
+++ .devcontainer/o1basics/dates.js
@@ -0,0 +1,32 @@
+// console.log(Date());
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+
+// console.log(typeof myDate);
+
+// let createDate = new Date(2023,0,25)
+let createdDate = new Date("01-1-2023")
+let createDate = new Date(2023,0,25,5,15)
+// console.log(createDate.toDateString());
+// console.log(createDate.toLocaleDateString());
+
+let timestanp = Date.now()
+
+// console.log(timestanp);
+// console.log(createdDate.getTime());
+// console.log(Math.floor(Date.now()/1000)); // Current second
+
+
+let newDate =  new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth() +1);
+// console.log(newDate.getHours());
+// console.log(newDate.getDay() +1);
+
+// newDate.toLocaleString('default',{
+//     weekDay:nar
+// })
