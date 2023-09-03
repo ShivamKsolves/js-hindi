7a74e1893cb3949093a0d3b62f6d62aa3c586faa
ShivamKsolves <113895013+ShivamKsolves@users.noreply.github.com>
2023-09-03 07:08:09 +0000

    String.js

---
+++ .devcontainer/o1basics/string.js
@@ -0,0 +1,34 @@
+// console.log('shivam');
+// const name="shivam"
+// const repoCount=50;
+
+// console.log(`Hello my name is ${name} and My repoCount is ${repoCount}`);
+
+const gameName = new String('shivam-kumar'); // String is an object
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('s'));
+
+const name = gameName.substring(0,4);
+// console.log(name);
+
+const anotherName = gameName.slice(0,4);
+// console.log(anotherName);
+
+
+
+const names= "shivam kumar"
+
+// console.log(names);
+// console.log(names.trim());
+
+const url= "https%shivam.com%20shivam"
+// console.log(url.replace("%20",'-'));
+// console.log(url.includes('shivam'));
+
+console.log(names.split(' '));
+
