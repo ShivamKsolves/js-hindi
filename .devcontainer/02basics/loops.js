6b14c7f7ac2e458f9788dc7b94e5094d5b14f8fb
ShivamKsolves <113895013+ShivamKsolves@users.noreply.github.com>
2023-09-04 03:13:50 +0000

    loops of javascript

---
+++ .devcontainer/02basics/loops.js
@@ -0,0 +1,29 @@
+// console.log('Loops... in javascript');
+// console.log('itterations in javascript...');
+
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if (element===5){
+//         console.log('5 is best Number.....');
+//     }
+//     console.log(element);
+    
+// }
+
+// console.log(index);
+
+
+for (let index = 0; index < 20; index++) {
+    const Number = index;
+    if (Number===10){
+        console.log('10 Number Detcted..');
+        break;
+    }
+    if (Number===5){
+        console.log('5 Number Detcted..');
+        continue;
+    }
+    console.log(`Number is ${Number}`);
+    
+}
